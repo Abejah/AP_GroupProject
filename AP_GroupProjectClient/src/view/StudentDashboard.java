bfefc9c001a360902cf841dab99b941a15821f6e
Del-Piero Graham <91176519+Del-PieroGraham@users.noreply.github.com>
2023-04-20 15:29:27 -0500

    Add files via upload

---
+++ AP_GroupProjectClient/src/view/StudentDashboard.java
@@ -0,0 +1,15 @@
+package view;
+
+import javax.swing.JFrame;
+
+
+			//student main menu
+
+public class StudentDashboard extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
