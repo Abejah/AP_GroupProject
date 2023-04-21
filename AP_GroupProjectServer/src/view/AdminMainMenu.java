256197cc0b95730d1ec8a241224641d3931d6734
Del-Piero Graham <91176519+Del-PieroGraham@users.noreply.github.com>
2023-04-20 23:05:01 -0500

    Add files via upload

---
+++ AP_GroupProjectServer/src/view/AdminMainMenu.java
@@ -0,0 +1,16 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class AdminMainMenu extends JFrame{
+
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ AP_GroupProjectServer/src/view/Student.java
@@ -0,0 +1,94 @@
+package view;
+
+import java.io.Serializable;
+
+public class Student implements Serializable{
+	private static final long serialVersionUID = 1L;
+private String firstName;
+private String lastName;
+private String userName;
+private String passowrd;
+private String email;
+private String mobileNumber;
+
+public Student() {
+	this.firstName = "";
+	this.lastName = "";
+	this.userName = "";
+	this.passowrd = "";
+	this.email = "";
+	this.mobileNumber = "";
+}
+
+public Student(String firstName, String lastName, String userName, String passowrd, String email,
+		String mobileNumber) {
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.userName = userName;
+	this.passowrd = passowrd;
+	this.email = email;
+	this.mobileNumber = mobileNumber;
+}
+
+public String getFirstName() {
+	return firstName;
+}
+
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+
+public String getLastName() {
+	return lastName;
+}
+
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+
+public String getUserName() {
+	return userName;
+}
+
+public void setUserName(String userName) {
+	this.userName = userName;
+}
+
+public String getPassowrd() {
+	return passowrd;
+}
+
+public void setPassowrd(String passowrd) {
+	this.passowrd = passowrd;
+}
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+public String getMobileNumber() {
+	return mobileNumber;
+}
+
+public void setMobileNumber(String mobileNumber) {
+	this.mobileNumber = mobileNumber;
+}
+
+@Override
+public String toString() {
+	return "First Name: " + firstName +
+			"\nLast Name: " + lastName + 
+			"\nUser Name: " + userName + 
+			"\nPassowrd: " + passowrd + 
+			"\nEmail: " + email + 
+			"\nMobile Number: " + mobileNumber + "\n";
+}
+
+
+
+
+}
